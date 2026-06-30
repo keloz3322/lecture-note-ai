08a8c3bca204de776bf3beed7092de986659d645
keloz <keloz3322@users.noreply.github.com>
2026-06-30 20:15:58 +0900

    Include ffmpeg binary in transcribe function

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,12 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  outputFileTracingIncludes: {
+    "/api/transcribe": [
+      "./node_modules/.pnpm/ffmpeg-static@*/node_modules/ffmpeg-static/ffmpeg",
+      "./node_modules/.pnpm/ffmpeg-static@*/node_modules/ffmpeg-static/ffmpeg.exe",
+    ],
+  },
 }
 
 export default nextConfig
